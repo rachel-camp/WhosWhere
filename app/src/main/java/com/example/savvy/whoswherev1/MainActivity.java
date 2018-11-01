7c2eaa34745aaa92f4d14657089edc19453dc90e
Michael <mgibson111@gmail.com>
2018-11-01 16:50:06 -0400

    Test commmit

--- app/src/main/java/com/example/savvy/whoswherev1/MainActivity.java
+++ app/src/main/java/com/example/savvy/whoswherev1/MainActivity.java
@@ -12,7 +12,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
+// TODO: test for commit
 public class MainActivity extends AppCompatActivity
 
         implements NavigationView.OnNavigationItemSelectedListener {
